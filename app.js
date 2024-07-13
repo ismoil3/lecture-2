5087ef0c7caaf3191326be32bb80967e81fec66c
unknown <you@example.com>
2024-07-13 09:22:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// Task1
+// let f=(a)=>{
+//     let fact = 1
+//     for(let i=1; i<=a; i++){
+//         fact *= i
+//     }
+//     return fact
+// }
+// console.log(f(5));
+
+
+// Task2
+// let f=(a)=>{
+//     let sum = 0
+//     for(let i=0; i<=a; i++){
+//         sum += i
+//     }
+//     return sum
+// }
+// console.log(f(10));
+
+
+// Task3
+// let f=(a, b)=>{
+//     return Math.pow(a, b)
+// }
+// console.log(f(2, 3));
+
+
+// Task4
+// function test(){
+//     let cnt = 0
+//     return()=>{
+//         cnt++
+//         return cnt
+//     }
+// }
+// let ans = test(1);
+// console.log(ans());  
+// console.log(ans());
+// console.log(ans());
+
+
+// Task5
+// function test(a){
+//     let sum = 0
+//     return function(b){
+//        sum = a+b
+//         return sum
+//     }
+// }
+// let ans = test(3);
+// console.log(ans(2));
